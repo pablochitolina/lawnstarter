acca0351415d0d406264ecd6f890d0e2fa65cba2
pchitolina <pchitolina@manta.com>
2026-01-13 20:25:56 -0300

    fix first run problens

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+# Install dependencies if missing
+if [ ! -d vendor ]; then
+    echo "Installing Composer dependencies..."
+    composer install --prefer-source --no-interaction
+fi
+
+# Copy .env if missing
+if [ ! -f .env ]; then
+    echo "Creating .env file..."
+    cp .env.example .env
+    php artisan key:generate
+fi
+
 # Setup SQLite database if it doesn't exist
 if [ ! -f database/database.sqlite ]; then
     echo "Creating database.sqlite..."
